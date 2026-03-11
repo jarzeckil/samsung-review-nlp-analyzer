de36d3a509a664b2ca0a86a143977df290bbec65
jarzeckil <jarzeckil@gmail.com>
2026-03-12 00:22:13 +0100

    feat: create func for retrieving from db

---
+++ src/retrieval.py
@@ -0,0 +1,7 @@
+from langchain_pinecone import PineconeVectorStore
+
+
+def retrieve_from_query(vector_store: PineconeVectorStore, query: str, k_results: int) -> list:
+    results = vector_store.similarity_search(query=query, k=k_results)
+
+    return results
