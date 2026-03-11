e4a9012f5ecb5d095c321cc38f4ed804aa42d668
jarzeckil <jarzeckil@gmail.com>
2026-03-12 00:56:28 +0100

    refactor: move prompt

--- src/core/config.py
+++ src/core/config.py
@@ -4,5 +4,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 DATA_DIR = PROJECT_ROOT / 'data'
 
+PROMPT_PATH = PROJECT_ROOT / 'prompts' / 'rag_system_prompt.txt'
+
 if __name__ == '__main__':
     print(PROJECT_ROOT)
