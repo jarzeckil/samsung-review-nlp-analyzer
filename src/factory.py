8fc25f079d518d6e343bd1824260a02ee1f6f9e7
jarzeckil <jarzeckil@gmail.com>
2026-03-12 00:23:20 +0100

    feat: create make model func

--- src/factory.py
+++ src/factory.py
@@ -1,17 +1,18 @@
 from langchain_core.embeddings import Embeddings
+from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
 
 def make_embeddings(
     model_name: str = 'sentence-transformers/all-mpnet-base-v2',
-    model_kwargs=None,
+    device='cpu',
     encode_kwargs=None,
 ) -> Embeddings:
     if encode_kwargs is None:
         encode_kwargs = {'normalize_embeddings': False}
-    if model_kwargs is None:
-        model_kwargs = {'device': 'cpu'}
+
+    model_kwargs = {'device': device}
 
     hf = HuggingFaceEmbeddings(
         model_name=model_name,
@@ -26,3 +27,13 @@ def make_vector_store(index_name: str, embeddings: Embeddings) -> PineconeVector
     vector_store = PineconeVectorStore(embedding=embeddings, index_name=index_name)
 
     return vector_store
+
+
+def make_model():
+    model = ChatGroq(
+        model='llama-3.1-8b-instant',
+        temperature=0.0,
+        max_retries=2,
+    )
+
+    return model
