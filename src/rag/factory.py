7ce4257df7c6afec119e7c0bd71ab40c0c186132
jarzeckil <jarzeckil@gmail.com>
2026-03-13 01:35:30 +0100

    feat: add ollama integration

--- src/rag/factory.py
+++ src/rag/factory.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from langchain.agents import create_agent
 from langchain_core.tools import tool
 from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
 from langchain_pinecone import PineconeVectorStore
 
 from src.core.config import PROMPT_PATH
@@ -49,11 +50,19 @@ def create_agent_tools(vector_store: PineconeVectorStore) -> list:
 
 
 def make_model():
-    model = ChatGroq(
-        model=os.getenv('CHAT_MODEL_NAME'),
-        temperature=0.0,
-        max_retries=2,
-    )
+    local = os.getenv('LOCAL_MODEL')
+
+    if local:
+        model = ChatOllama(
+            model=os.getenv('OLLAMA_MODEL_NAME'),
+            temperature=0.0,
+        )
+    else:
+        model = ChatGroq(
+            model=os.getenv('GROQ_MODEL_NAME'),
+            temperature=0.0,
+            max_retries=2,
+        )
 
     return model
 
